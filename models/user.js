bf20aa31953a445a413afcbe4992279a7343a613
ashishpal07 <ashishpal7861@gmail.com>
2023-03-27 13:28:23 +0530

    created correct uesr schema

--- models/user.js
+++ models/user.js
@@ -2,26 +2,30 @@ const mongoose = require('mongoose'); // Erase if already required
 
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        unique:true,
-        index:true,
+
+    firstName: {
+        type: String,
+        required: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
+    lastName: {
+        type: String,
+        required: true,
     },
-    mobile:{
-        type:String,
-        required:true,
-        unique:true,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    mobile: {
+        type: String,
+        required: true,
+        unique: true,
     },
     password:{
-        type:String,
-        required:true,
-    },
+        type: String,
+        required: true,
+    }
+
 });
 
 //Export the model
